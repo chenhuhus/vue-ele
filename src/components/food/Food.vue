0a7090b9792df80c58499474a4e55f2e7a34715f
chenhhs <chenhhs@outlook.com>
2017-03-10 16:33:51 +0800

    goods food

--- src/components/food/Food.vue
+++ src/components/food/Food.vue
@@ -17,19 +17,26 @@
           <div class="price">
             <span class="now">{{food.price}}</span><span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
         </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food"></cartcontrol>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品介绍</h1>
+          <p class="text">{{food.info}}</p>
         </div>
-        <div class="buy" v-show="!food.count || food.count ===0"></div>
       </div>
     </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import Vue from 'vue'
   import BScroll from 'better-scroll'
   import Cartcontrol from 'components/cartcontrol/Cartcontrol'
+  import Split from 'components/split/Split'
 
   export default {
     props: {
@@ -57,10 +64,17 @@
       },
       hide () {
         this.showFlag = false
+      },
+      addFirst (event) {
+        if (!event._constructed) {
+          return
+        }
+        Vue.set(this.food, 'count', 1)
       }
     },
     components: {
-      Cartcontrol
+      Cartcontrol,
+      Split
     }
   }
 </script>
@@ -115,6 +129,7 @@
 
     .content {
       padding: 18px;
+      position: relative;
 
       .title {
         line-height: 14px;
@@ -123,6 +138,7 @@
         font-weight: 700;
         color: rgb(7, 17, 27);
       }
+
       .detail {
         margin-bottom: 18px;
         line-height: 10px;
@@ -154,7 +170,45 @@
           color: rgb(147, 153, 159);
         }
       }
+
+      .cartcontrol-wrapper {
+        position: absolute;
+        right: 12px;
+        bottom: 12px;
+      }
+
+      .buy {
+        position: absolute;
+        right: 18px;
+        bottom: 18px;
+        z-index: 10;
+        height: 24px;
+        line-height: 24px;
+        padding: 0 12px;
+        box-sizing: border-box;
+        border-radius: 12px;
+        font-size: 10px;
+        color: #fff;
+        background: rgb(0, 160, 220);
+      }
     }
 
+    .info {
+      padding: 0 18px;
+
+      .title {
+        line-height: 14px;
+        margin-top: 6px;
+        font-size: 14px;
+        color: rgb(7, 17, 27);
+      }
+
+      .text {
+        line-height: 24px;
+        padding: 0 8px;
+        font-size: 12px;
+        color: rgb(77, 85, 93)
+      }
+    }
   }
 </style>
---
+++ src/components/split/Split.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="split"></div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .split {
+    width: 100%;
+    height: 16px;
+    border-top: 1px solid rgba(7, 17, 27, 0.1);
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+    background: #F3F5F7;
+  }
+</style>
